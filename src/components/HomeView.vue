d5121c058685ccc845ff3fb5086e59de2ca7e837
jydoskey <jideomotola33@gmail.com>
2022-08-09 21:53:35 +0100

    created default chat view

--- src/components/HomeView.vue
+++ src/components/HomeView.vue
@@ -1,8 +1,20 @@
 <template>
   <main>
-    
+    <div class="phone-body">
+      <div>
+        <img class="left-icon" src="@/assets/user-profile-icon-left.svg" alt="profile icon">
+      </div>
+    </div>
   </main>
 </template>
 
-<script>
-</script>
+<style>
+.phone-body {
+  @apply w-[360px] h-screen border border-solid border-[#9f2ae8] border-t-4 p-2;
+}
+
+.left-icon {
+  @apply w-12;
+}
+
+</style>
