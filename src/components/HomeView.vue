b9d1ebe934b77d81eaa3c065f1087e16f42909ee
jydoskey <jideomotola33@gmail.com>
2022-08-09 22:38:23 +0100

    added new chat texts

--- src/components/HomeView.vue
+++ src/components/HomeView.vue
@@ -16,7 +16,7 @@
       <div class="chat-view">
         <div class="chat-box bg-[#d3d3d3] triangle right-top">
           <div class="chat-text">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+            <p>Lorem ipsum dolor sit amet, consectetur</p>
           </div>
         </div>
         <img
@@ -24,6 +24,30 @@
           src="@/assets/user-profile-icon-right.svg"
           alt="profile icon"
         />
+      </div>
+       <div class="chat-view">
+        <div class="chat-box bg-[#d3d3d3] triangle right-top">
+          <div class="chat-text">
+            <p>Lorem ipsum dolor</p>
+          </div>
+        </div>
+        <img
+          class="left-icon"
+          src="@/assets/user-profile-icon-right.svg"
+          alt="profile icon"
+        />
+      </div>
+      <div class="chat-view">
+        <img
+          class="left-icon"
+          src="@/assets/user-profile-icon-left.svg"
+          alt="profile icon"
+        />
+        <div class="chat-box bg-[#9f2ae8] triangle left-top">
+          <div class="chat-text">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing</p>
+          </div>
+        </div>
       </div>
     </div>
   </main>
@@ -47,15 +71,13 @@
 }
 
 .triangle.left-top:after {
-  content: " ";
-  @apply w-0 h-0 top-0 right-auto -left-5 bottom-auto absolute;
+  @apply w-0 h-0 top-0 right-auto -left-5 bottom-auto absolute content-none;
   border: 22px solid;
   border-color: #9f2ae8 transparent transparent transparent;
 }
 
 .triangle.right-top:before {
-  content: " ";
-  @apply w-0 h-0 top-0 -right-5 left-auto bottom-auto absolute;
+  @apply w-0 h-0 top-0 -right-5 left-auto bottom-auto absolute content-none;
   border: 32px solid;
   border-color: #d3d3d3 transparent transparent transparent;
 }
